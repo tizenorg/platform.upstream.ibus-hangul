c894c85869ca9295bd03e2f868f3aa8a33f92999
Eduardo Lima (Etrunko) <eduardo.lima@intel.com>
2013-12-10 14:40:00 -0200

    Imported Upstream version 1.4.2

---
+++ setup/config.py
@@ -0,0 +1,26 @@
+# vim:set et sts=4 sw=4:
+#
+# ibus-hangul - The Hangul Engine For IBus
+#
+# Copyright (c) 2009-2011 Choe Hwanjin <choe.hwanjin@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+gettext_package = 'ibus-hangul'
+localedir = '/usr/local/share/locale'
+pkgdatadir = '/usr/local/share/ibus-hangul'
+pkglibdir = '/usr/local/lib/ibus-hangul'
+setupdatadir = pkgdatadir + '/setup'
+setuplibdir = pkglibdir + '/setup'
